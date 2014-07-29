2e8daf8fb6622b3a61325656d73bacd797cc0bf9
yqh <392757759@qq.com>
2014-07-29 11:14:52 +0800

    finish

--- src/com/missedcall/activity/MissedCallActivity.java
+++ src/com/missedcall/activity/MissedCallActivity.java
@@ -1,9 +1,20 @@
 package com.missedcall.activity;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import com.example.missedcallrecord.R;
+import com.missedcall.service.PhoneService;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -22,17 +33,55 @@ public class MissedCallActivity extends Activity{
         button = (Button) findViewById(R.id.startservice);
         content.setText(s);
         button.setOnClickListener(new OnClickListener() {
-            
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
-                
+                Intent intent = new Intent();
+                intent.setClass(MissedCallActivity.this, PhoneService.class);
+                startService(intent);
+                MissedCallActivity.this.finish();
             }
         });
     }
     private String getfilecontent() {
         // TODO Auto-generated method stub
-        return null;
+        String filepath = null;
+        if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
+            filepath = Environment.getExternalStorageDirectory() +"/android/data/"
+        +getPackageName()+"/content.txt";
+        }else
+            filepath = Environment.getDataDirectory().toString() + "/data/"
+            +getPackageName()+"/content.txt";
+        Log.e("filepath", filepath);
+        File file = new File(filepath);
+        if(!file.exists()){
+            try {
+                Log.e("file", "not exists");
+                file.createNewFile();
+                return null;
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        String content = "";
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String temp;
+            while((temp=reader.readLine())!=null){
+                    content = content +temp;
+            }
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        if (content == "")
+            return "Nothing";
+        else
+            return content;
     }
 
 }
--- src/com/missedcall/service/PhoneService.java
+++ src/com/missedcall/service/PhoneService.java
@@ -1,16 +1,26 @@
 package com.missedcall.service;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Date;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Environment;
 import android.os.IBinder;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 
 
 public class PhoneService extends Service{
-
+    private boolean flag = false;
+    Date from = null, end = null;
     @Override
     public IBinder onBind(Intent intent) {
         // TODO Auto-generated method stub
@@ -27,24 +37,26 @@ public class PhoneService extends Service{
     public void onStart(Intent intent, int startId) {
         // TODO Auto-generated method stub
         super.onStart(intent, startId);
+        TelephonyManager manager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
         PhoneStateListener listener = new PhoneStateListener(){
-
             @Override
             public void onCallStateChanged(int state, String incomingNumber) {
                 // TODO Auto-generated method stub
-                boolean flag = false;
-                Date from = null, end = null;
                 switch (state) {
                 case TelephonyManager.CALL_STATE_IDLE:
+                    Log.e("filepath", "idle1");
                     if(flag){
+                        Log.e("filepath", "idle");
                         end = new Date();
                         insertinfo(from, end, incomingNumber);
                         }
                     break;
                 case TelephonyManager.CALL_STATE_OFFHOOK:
+                    Log.e("filepath", "offhook");
                     flag = false;
                     break;
                 case TelephonyManager.CALL_STATE_RINGING:
+                    Log.e("filepath", "ringing");
                     from = new Date();
                     flag = true;
                     break;
@@ -54,15 +66,43 @@ public class PhoneService extends Service{
                 super.onCallStateChanged(state, incomingNumber);
             }
         };
+        manager.listen(listener, PhoneStateListener.LISTEN_CALL_STATE);
     }
 
     protected void insertinfo(Date from, Date end, String number) {
         // TODO Auto-generated method stub
-        String filepath;
+        String filepath = null;
         if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
-            filepath = Environment.getExternalStorageDirectory() +"/android/data"
-        +getPackageName()+"/content.text";
+            filepath = Environment.getExternalStorageDirectory() +"/android/data/"
+        +getPackageName()+"/content.txt";
+            
+        }else
+            filepath = Environment.getDataDirectory().toString() + "/data/"
+            +getPackageName()+"/content.txt";
+        Log.e("filepath", filepath);
+        File file = new File(filepath);
+        if(!file.exists()){
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
+        int second = end.getSeconds()-from.getSeconds()+60*(end.getMinutes()-from.getMinutes());
+        try {
+            FileWriter writer = new FileWriter(file);
+            writer.append("\tFrom:"+number+"\tDate_from:"+from+"\tDate_end:"+end+"\n"+"\tDuring:"+second+"s\n");
+            Log.e("file", "write");
+            writer.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
     }
     
 }
